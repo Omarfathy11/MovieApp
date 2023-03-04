3a23425ee70e7d2383437b6f9a3ebee182d695c4
hady eslam <fathy11@gmail.com>
2023-03-04 21:17:05 +0200

    Adding postgresdb

--- MovieApp/MovieApp/settings.py
+++ MovieApp/MovieApp/settings.py
@@ -76,10 +76,25 @@ WSGI_APPLICATION = 'MovieApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'lSyVn9xpPn1gSCSFBREh',
+        'HOST': 'containers-us-west-48.railway.app',
+        'PORT': '7513',
+        # 'TEST': {
+        #     'NAME': '',
+        # },
     }
 }
 
